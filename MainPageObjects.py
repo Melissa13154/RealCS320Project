e803df0bd051e1dce28be85a801be62a4c7329a6
Melissa13154 <melissa.matteo@wsu.edu>
2024-02-02 14:32:12 -0800

    Trying again
    I thought I added this already? Same as last commit

--- MainPageObjects.py
+++ MainPageObjects.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import Canvas
 from tkinter import PhotoImage
+from datetime import datetime
 
 backgroundColor = "#D1FFB0"
 
 class StartingLabel:
     def __init__(self, root):
         self.root = root
-        self.label = tk.Label(self.root, text = "Click button below to start timer", font=('MS Sans Serif', 20), bg= backgroundColor)
+        self.label = tk.Label(self.root, text = "Enter a time and click start", font=('MS Sans Serif', 20), bg= backgroundColor)
         self.label.place(relx=.5, rely=.1, anchor="center")
 
 
@@ -22,15 +23,40 @@ class StartButton:
     def handleMouseClick(self):
         self.clicks = self.clicks+1
 
-        if((self.clicks)%2 != 0):
+        if(self.clicks%2 != 0):
             self.button.config(text = "Stop")
         else:
             self.button.config(text = "Start")
+
+            
+
+class TimerTimeOptions:
+    def __init__(self, root):
+        self.root = root
+
+        self.timeOptions = ('00:10:00',
+                            '00:15:00',
+                            '00:20:00',
+                            '00:30:00',
+                            '00:45:00',
+                            '01:00:00')
+
+        self.menu = tk.OptionMenu(self.root, tk.StringVar(self.root), self.timeOptions[0], *self.timeOptions)
+        self.menu.pack() #just throwing it in for now
+
 
 
 class Timer:
     def __init__(self, root):
         self.root = root
-        self.label = tk.Label(self.root, text="00:00", font=('MS Sans Serif', 20), bg=backgroundColor)
+
+        self.timeLeft = "00-15-00"
+        self.timeLeftLabel = str(datetime.strptime(self.timeLeft, "%H-%M-%S"))
+        self.timeLeftLabel = self.timeLeftLabel.split()
+        self.timeLeftLabel = self.timeLeftLabel[1]
+        
+        self.label = tk.Label(self.root, text=self.timeLeftLabel, font=('MS Sans Serif', 20), bg=backgroundColor)
         self.label.place(relx= 0.5, rely=0.75, anchor='center')
 
+    def countDown(self):
+        pass
