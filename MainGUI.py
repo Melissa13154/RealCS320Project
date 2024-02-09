ffabc80a3205b5b6f0165cd962126c175e43e72e
Venessa <124633733+VenessaJK@users.noreply.github.com>
2024-02-09 12:40:55 -0800

    takes user input in entrybox

--- MainGUI.py
+++ MainGUI.py
@@ -4,6 +4,7 @@ from tkinter import PhotoImage
 
 import MainPageObjects
 import TimerObject
+import TimerTags
 
 backgroundColor = "#D1FFB0"
 
@@ -19,12 +20,13 @@ class MainFrame:
         #image of clock    
         self.image1 = Canvas(self.root, width =350, height=350)
         self.image1.place(relx=.5, rely=.4, anchor="center")
-        self.photo1 = PhotoImage(file='Images/Clock2-resized.gif') #TODO: Figure out how to make corners transparent
+        self.photo1 = PhotoImage(file='Clock.gif') #TODO: Figure out how to make corners transparent
         self.image1.create_image((1, 1), anchor='nw',image= self.photo1)
 
 main = MainFrame()
 mainLabel = MainPageObjects.StartingLabel(main.root)
 timer = TimerObject.Timer(main.root)
 goalDashboardButton = MainPageObjects.GoalDashboardButton(main.root)
+tags = TimerTags.Tags(main.root)
 
 main.root.mainloop() 
