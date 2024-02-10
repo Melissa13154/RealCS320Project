f7685f4953cacdd0721a2a6930044210386c5063
Linnea Castro <linneapaulina@gmail.com>
2024-02-09 16:27:08 -0800

    Update MainGUI.py with Notebook framework.

--- MainGUI.py
+++ MainGUI.py
@@ -1,3 +1,4 @@
+### IMPORTS ###
 import tkinter as tk
 from tkinter import Canvas
 from tkinter import PhotoImage
@@ -8,18 +9,45 @@ import MainPageObjects
 import TimerObject
 import TimerFrameObject
 
+### DETAILS ###
+TK_SILENCE_DEPRECATION=1 # Supress warnings
 backgroundColor = "#3A7069"
 
-TK_SILENCE_DEPRECATION=1 #suppress warnings
+### OUTERFRAME CLASS  ###
+class OuterFrame:
+    root = tk.Tk() # Creates main root
+    root.title('Our Wonderful Time Tracking GUI 1.0.0') # Title displays at the top
+    #root.geometry = ('500 x 700') # Initial dimensions of window
+    root.minsize(500, 700) # Hmm, this one seems to work and not the root.geometry statement above
 
-class MainFrame(tk.Tk):
-    def __init__(self):
-        #self.root = ?
-        container = tk.Frame(self)
+    ### CREATE NOTEBOOK ###
+    notebook = ttk.Notebook(root) # Create Notebook
 
-noteBock = MainFrame()
-TimerFrame = TimerFrameObject.TimerFrame(MainFrame.root)
-timer = TimerObject.Timer(MainFrame.root)
-goalDashboardButton = MainPageObjects.GoalDashboardButton(MainFrame.root)
+    ### CREATE TABS ###
+    mainTab = ttk.Frame(notebook)
+    tagsTab = ttk.Frame(notebook)
+    goalsTab = ttk.Frame(notebook)
 
-main.root.mainloop() 
+    ### POPULATE NOTEBOOK WITH TABS ###
+    notebook.add(mainTab, text = "Main Menu")
+    notebook.add(tagsTab, text = "Tags Menu")
+    notebook.add(goalsTab, text = "Goal Dashboard")
+
+    notebook.pack(expand = 1, fill = 'both')
+
+    ### ADD TO mainTab ###
+    Label(mainTab, text = "Put frame with timer, clock image, start/stop button here.").pack()
+    # TODO: Add more widgets here : https://docs.python.org/3/library/tkinter.ttk.html
+    # Add TimerObject, TimerFrameObjects, etc.  Will this be a possible?
+
+    ### ADD TO tagsTab ###
+    Label(tagsTab, text = "Make new tags here.").pack()
+
+    ### ADD TO goalsTab ###
+    Label(goalsTab, text = "Goals Dashboard here.").pack()
+
+### CREATE OBJECT ###
+outerFrame = OuterFrame()
+
+### MAINLOOP CALL ###
+outerFrame.root.mainloop()
